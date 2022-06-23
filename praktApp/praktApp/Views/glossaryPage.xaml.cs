888b6d6d1940e2d3d79715da208372fc73435517
disai <di-saifutdinova@bk.ru>
2022-06-23 22:09:06 +0300

    Добавьте файлы проекта.

---
+++ praktApp/praktApp/Views/glossaryPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace praktApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class glossaryPage : ContentPage
+    {
+        public glossaryPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ praktApp/praktApp/Views/menuPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace praktApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class menuPage : ContentPage
+    {
+        public menuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+
+        }
+    }
+}

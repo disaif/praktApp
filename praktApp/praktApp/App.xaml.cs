f9f23eca540d0d45ad317cde4a0fe16b9d6c9c4b
Никита Кравченко <KravchenkoNA@stud.kai.ru>
2022-06-25 11:59:54 +0300

    Картинки обновил

--- praktApp/praktApp/App.xaml.cs
+++ praktApp/praktApp/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using praktApp.Data;
+using praktApp.Models;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using testAnd;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -17,6 +20,24 @@ namespace praktApp
                 if (praktDB == null)
                 {
                     praktDB = new PraktDB(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "HRDatabase.db3"));
+
+
+                    Category categoryME = new Category() { IsPublic = true, Name = "Мэкэникал Инжинеринг"};
+                    praktDB.SaveCategoryAsync(categoryME);
+                    categoryME = praktDB.GetCategoryAsync().Result.ToList().FirstOrDefault();
+                    List<Word> words = new List<Word>(){ 
+                    new Word() { Category = categoryME, CategoryId = categoryME.Id, Term = "involves", Translation = "Включать"},
+                    new Word() { Category = categoryME, CategoryId = categoryME.Id, Term = "maintence", Translation = "эксплуатация"},
+                    new Word() { Category = categoryME, CategoryId = categoryME.Id, Term = "request", Translation = "требовать"},
+                    new Word() { Category = categoryME, CategoryId = categoryME.Id, Term = "predict", Translation = "предсказывать"},
+                    new Word() { Category = categoryME, CategoryId = categoryME.Id, Term = "employ", Translation = "frame"},
+                    };
+                    foreach (Word word in words)
+                        praktDB.SaveWordAsync(word);
+                    categoryME.Words = words;
+                    praktDB.SaveCategoryAsync(categoryME);
+
+
                 }
                 return praktDB;
 
--- praktApp/praktApp/Views/menuPage.xaml.cs
+++ praktApp/praktApp/Views/menuPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using praktApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,11 @@ namespace praktApp.Views
         public menuPage()
         {
             InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            List<Category> category = await App.PraktDB.GetCategoryAsync();
         }
 
         private void Switch_Toggled(object sender, ToggledEventArgs e)

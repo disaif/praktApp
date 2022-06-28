c608788bf766e162bb16eae5f2d180c9870b6095
disai <di-saifutdinova@bk.ru>
2022-06-29 02:59:22 +0300

    бедни

--- praktApp/praktApp/Views/menuPage.xaml.cs
+++ praktApp/praktApp/Views/menuPage.xaml.cs
@@ -1,14 +1,13 @@
-﻿using praktApp.Data;
-using praktApp.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using praktApp.Helpers;
+using praktApp.Data;
+using praktApp.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using praktApp.Helpers;
 
 namespace praktApp.Views
 {
@@ -33,40 +32,14 @@ namespace praktApp.Views
             }
         }
         bool loaded;
-   
+
         private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new settingsPage());
         }
 
-        private async void Button_Clicked(object sender, EventArgs e)
-        {
-            if (App.CurrentUser == null)
-            {
-                await Navigation.PushAsync(new AutorizationPage());
-            }
-            else
-            {
-                App.CurrentUser = null;
-                SaveClass.DeleteAll();
-                ButEnt.Text = "войти";
-                ImageU.Source = ImageSource.FromResource("praktApp.Images.profile.png");
-            }
-        }
-
         protected override void OnAppearing()
         {
-            if (App.CurrentUser != null)
-            {
-                ButEnt.Text = "выход";
-                if (App.CurrentUser.Photo != null)
-                {
-                    var stream1 = new MemoryStream(App.CurrentUser.Photo);
-                    ImageU.Source = ImageSource.FromStream(() => stream1);
-                }
-            }
-            else
-                ButEnt.Text = "войти";
             loaded = true;
         }
 

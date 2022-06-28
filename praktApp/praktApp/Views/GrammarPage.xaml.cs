2c42768f2c905ac64411143538b96a8fc63f6fb8
disai <di-saifutdinova@bk.ru>
2022-06-28 20:30:50 +0300

    начало учебника ура

---
+++ praktApp/praktApp/Views/GrammarPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace praktApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GrammarPage : ContentPage
+    {
+        public GrammarPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TapGestureRecognizer_Tapped_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
